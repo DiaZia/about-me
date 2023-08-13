6c1fd225e07b2e2d22c1e6dfd837d14dce114fef
Diana Žiaková <dianaziakova@gmail.com>
2023-08-13 18:53:01 +0200

    Add click functions to open links

--- script.js
+++ script.js
@@ -13,7 +13,28 @@ function moveImage() {
 
 setInterval(moveImage, 15);
 
+ $('#planner').click(function() {
+        var linkUrl = 'https://github.com/DiaZia/Workout-planner-app';
+        window.open(linkUrl, '_blank');
+});
+
+$('#game').click(function() {
+    var linkUrl = 'https://github.com/DiaZia/Balloons-typing-game';
+    window.open(linkUrl, '_blank');
+});
+
+$('#calculator').click(function() {
+    var linkUrl = 'https://diazia.github.io/calculator-web-app/';
+    window.open(linkUrl, '_blank');
+});
+
+$('#linkedin').click(function() {
+    var linkUrl = 'https://www.linkedin.com/in/diana-%C5%BEiakov%C3%A1-115ba9280/';
+    window.open(linkUrl, '_blank');
+});
 
 
-
-
+$('#github').click(function() {
+    var linkUrl = 'https://github.com/DiaZia/portfolio';
+    window.open(linkUrl, '_blank');
+});
